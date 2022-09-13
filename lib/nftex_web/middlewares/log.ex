dd1ec88af3f55d0ea6ce1b2601d64709e14dfb34
neanderdev <neander.faria@hotmail.com>
2022-09-13 10:43:45 -0300

    Terminando o middleware de Log

--- lib/nftex_web/middlewares/log.ex
+++ lib/nftex_web/middlewares/log.ex
@@ -3,9 +3,26 @@ defmodule NftexWeb.Middlewares.Log do
 
   @behaviour Absinthe.Middleware
 
+  @filter_string "[FILTERED]"
+  @replacements [
+    {~r/hash: "\w*"/, "hash: #{@filter_string}"},
+    {~r/artist_name: "\w*"/, "artist_name: #{@filter_string}"}
+  ]
+
   def call(resolution, _) do
-    Logger.info(resolution.value)
+    resolution
+    |> log_message()
+    |> filter_info()
+    |> Logger.debug()
 
     resolution
+  end
+
+  defp log_message(%Absinthe.Resolution{value: value}), do: "Resolution value #{inspect(value)}"
+
+  defp filter_info(log) do
+    Enum.reduce(@replacements, log, fn {regex, replacement}, log ->
+      String.replace(log, regex, replacement)
+    end)
   end
 end
